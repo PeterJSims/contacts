cd263aa33feffa54796b8985ce30884d68e2c97b
Peter Sims <ragtimesims@gmail.com>
2019-08-13 17:53:53 -0400

    authentication route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -37,12 +40,26 @@ router.post(
 			});
 
 			const salt = await bcrypt.genSalt(10);
-
 			user.password = await bcrypt.hash(password, salt);
-
 			await user.save();
 
-			res.send('User saved');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (error) {
 			console.error(err.message);
 			res.status(500).send('Server Error');
