ca08474d3b9306ebe0c54da250421a1041aacff4
Peter Sims <ragtimesims@gmail.com>
2019-08-15 16:57:30 -0400

    register and login

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,14 +7,21 @@ const Navbar = ({ title, icon }) => {
 		<div className="navbar bg-primary">
 			<h1>
 				<Link to="/">
-					<i className={icon} />
-				</Link>{' '}
-				{title}
+					<i className={icon} /> {title}
+				</Link>
 			</h1>
 			<ul>
 				<li>
 					<Link to="/">Home</Link>
+				</li>
+				<li>
 					<Link to="/about">About</Link>
+				</li>
+				<li>
+					<Link to="/register">Register</Link>
+				</li>
+				<li>
+					<Link to="/login">Login</Link>
 				</li>
 			</ul>
 		</div>
